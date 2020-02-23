1d90c4985d7dd0950d677746658202f3c8e64cce
saralotano <saralotano@gmail.com>
2020-02-23 05:27:50 -0500

    Added python scripts

---
+++ python/network_topology.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Node, RemoteController
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+class LinuxRouter( Node ):
+	"A Node with priority and IP forwarding enabled."
+
+	def __init__( self, name, prio, **params ):
+		self.priority = prio
+		Node.__init__( self, name, **params )
+
+	def config( self, **params ):
+		super( LinuxRouter, self).config( **params )
+		self.cmd('sysctl net.ipv4.ip_forward=1')  # Enable forwarding
+		self.cmd( 'python3 router.py '+self.name + ' ' + str(self.priority) + '&')
+
+	def terminate( self ):
+		self.cmd('sysctl net.ipv4.ip_forward=0')
+		super( LinuxRouter, self ).terminate()
+
+
+class NetworkTopology( Topo ):
+	"Network composed by two Subnets connected by two LinuxRouters"
+
+	def build( self, **_opts ):
+
+		# Adding routers
+		r1 = self.addNode('R1', cls=LinuxRouter, prio=255, ip='10.0.1.2/24')
+		r2 = self.addNode('R2', cls=LinuxRouter, prio=100, ip='10.0.1.3/24')
+
+		# Adding hosts
+		h1 = self.addHost('H1', ip='10.0.1.4/24', defaultRoute='via 10.0.1.1')
+		h2 = self.addHost('H2', ip='10.0.1.5/24', defaultRoute='via 10.0.1.1')
+		h3 = self.addHost('H3', ip='10.0.1.6/24', defaultRoute='via 10.0.1.1')
+		h4 = self.addHost('H4', ip='10.0.2.3/24', defaultRoute='via 10.0.2.1')
+		h5 = self.addHost('H5', ip='10.0.2.4/24', defaultRoute='via 10.0.2.1')
+
+		# Adding SDN-enabled switch
+		s1 = self.addSwitch('S1')
+		# Adding Legacy switch
+		s2 = self.addSwitch('S2')
+
+		# Adding links between network components
+		self.addLink(s1, r1, intfName2='r1-eth0', params2={'ip' : '10.0.1.2/24'})
+		self.addLink(s1, r2, intfName2='r2-eth0', params2={'ip' : '10.0.1.3/24'})
+		self.addLink(h1, s1)
+		self.addLink(h2, s1)
+		self.addLink(h3, s1)		
+		self.addLink(s2, r1, intfName2='r1-eth1', params2={'ip' : '10.0.2.1/24'})
+		self.addLink(s2, r2, intfName2='r2-eth1', params2={'ip' : '10.0.2.2/24'})
+		self.addLink(h4, s2)
+		self.addLink(h5, s2)
+
+
+def run():
+
+	topo = NetworkTopology()
+	net = Mininet( topo=topo , controller=lambda name: RemoteController( name, ip='127.0.0.1' ))  # controller is used by s1
+
+	net.start()
+	CLI( net )
+	net.stop()
+
+if __name__ == '__main__':
+	setLogLevel( 'info' )
+	run()
---
+++ python/router.py
@@ -0,0 +1,33 @@
+import socket
+import time
+import sys
+
+UPD_BROADCAST_IP = '10.0.1.255'
+UPD_PORT = 1234
+ADVERTISEMENT_INTERVAL = 1 	
+
+
+if len(sys.argv) != 3:
+    print('Error: wrong number of parameters.')
+
+else:
+    router_parameters = sys.argv[1]+':'+ sys.argv[2]
+
+    # Create a new socket using the given address family and socket type
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # socket.SO_REUSEPORT: enable port reusage so we will be able to run multiple clients and servers on single (host, port).
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+    # socket.SO_BROADCAST: this option controls whether datagrams may be broadcast from the socket.
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    message = bytes(router_parameters,encoding='utf8')
+
+    print('Sending router name: ' + sys.argv[1] + ' and priority: ' + sys.argv[2])
+
+    while True:
+        print("Sending keep alive message")
+        # Send keep alive message each ADVERTISEMENT_INTERVAL seconds
+        s.sendto(message, (UPD_BROADCAST_IP, UPD_PORT))
+        time.sleep(ADVERTISEMENT_INTERVAL)
